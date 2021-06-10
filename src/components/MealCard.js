967c64f8024af24c908ce11d1e9af396fde4cdae
Chase Lones <chaselones@gmail.com>
2021-06-10 10:54:51 -0700

    wrapped links in router
    basename={process.env.PUBLIC_URL}

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import { Card, CardImg, CardText, CardBody, CardTitle, button  } from 'reactstrap'
-import {Link} from 'react-router-dom'
+import React from 'react'
+import { Card, CardImg, CardText, CardBody, CardTitle, button } from 'reactstrap'
+import { Link, BrowserRouter as Router } from 'react-router-dom'
 
 // props needed for meal cards:
 // title
@@ -8,25 +8,27 @@ import {Link} from 'react-router-dom'
 // category
 // key
 
-const MealCard = (props)=>{
+const MealCard = (props) => {
     console.log(props.title)
 
- return(
-    <div className = "col-sm-4 mt-3 mb-3">
-    <Card style={{ width: '100%', textAlign: "center" }}>
-        <CardTitle><h5>{props.title}</h5></CardTitle>
-        <CardBody>
-            <CardImg variant="top" src={props.imageLocation} />
-            <CardText className="mt-2">
-                <h5>{props.category}</h5>
-            </CardText>
-            <Link to={`/recipe/${props.id}`}>
-                    <button className="btn btn-outline-secondary recipeButton"> Recipe</button>
-            </Link>
-        </CardBody>
-    </Card>
-    </div>
- )
+    return (
+        <div className="col-sm-4 mt-3 mb-3">
+            <Card style={{ width: '100%', textAlign: "center" }}>
+                <CardTitle><h5>{props.title}</h5></CardTitle>
+                <CardBody>
+                    <CardImg variant="top" src={props.imageLocation} />
+                    <CardText className="mt-2">
+                        <h5>{props.category}</h5>
+                    </CardText>
+                    <Router basename={process.env.PUBLIC_URL} >
+                        <Link to={`/recipe/${props.id}`}>
+                            <button className="btn btn-outline-secondary recipeButton"> Recipe</button>
+                        </Link>
+                    </Router>
+                </CardBody>
+            </Card>
+        </div>
+    )
 }
 
 export default MealCard
